3969110573e0b5463f1dc6f70302b3d15c8d5369
unknown <pitbullali777@gmail.com>
2019-05-27 17:28:11 +0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <pie-chart :data="chartData" :option="options"></pie-chart>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PieChart from "./PieChart.js";
 export default {
-  name: 'app',
+  name: "App",
   components: {
-    HelloWorld
+    PieChart
+  },
+  data() {
+    return {
+      chartData: {
+        labels: ["Green", "Red", "Blue"],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: ["#41B883", "#E46651", "#00D8FF"],
+            data: [1, 10, 5]
+          }
+        ]
+      },
+      options:{
+            zoomOutPercentage: 20, // makes chart 40% smaller (50% by default, if the preoprty is undefined)
+            plugins: {
+                legend: false,
+                outlabels: {
+                    text: '%l',
+                    color: 'white',
+                    stretch: 30,
+                    font: {
+                        resizable: true,
+                        minSize: 12,
+                        maxSize: 18
+                    }
+                }
+            }
+        },
+      responsive:true,
+    };
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
